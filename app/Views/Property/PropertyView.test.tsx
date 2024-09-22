28402fb3ee5302c24da91ca755a4feea047f458f
emrekardaslar <emre.kardaslar@gmail.com>
2024-09-22 15:51:40 +0300

    simplify view test, add reducer test

--- app/Views/Property/PropertyView.test.tsx
+++ app/Views/Property/PropertyView.test.tsx
@@ -1,16 +1,11 @@
 import { useReducer } from "react";
 import { render, screen, fireEvent } from "@testing-library/react";
 import { StateProvider } from "../../context/PropertyContext";
-import { State } from "../../context/types";
 import { reducer } from "../../reducers/PropertyReducer";
 import PropertyView from "./PropertyView";
-import { describe, it, expect, vi } from "vitest";
-import {
-  ACTION_TYPE_ONE,
-  ACTION_TYPE_TWO,
-} from "../../actions/PropertyActions";
+import { describe, it, expect } from "vitest";
 
-const initialState: State = {
+const initialState = {
   propertyOne: 0,
   propertyTwo: 0,
 };
@@ -47,17 +42,10 @@ describe("PropertyView", () => {
     );
 
     const updateButton = screen.getAllByText(/Update Property One/i)[0];
-    fireEvent.click(updateButton); // Click the first button
+    fireEvent.click(updateButton);
 
-    const propertyOneElement = screen.getAllByText(/Property One:/i)[0];
-    expect(propertyOneElement).toHaveTextContent("Property One: -100"); // Adjust based on your reducer logic
-
-    // Assert the updated state
-    const stateAfterUpdate = reducer(initialState, {
-      type: ACTION_TYPE_ONE,
-      payload: -100,
-    });
-    expect(stateAfterUpdate.propertyOne).toBe(-100); // Expected value after update
+    const propertyOneElements = screen.getAllByText(/Property One:/i);
+    expect(propertyOneElements[0]).toHaveTextContent("-100"); // Adjust based on your reducer logic
   });
 
   it("updates Property Two when button is clicked", () => {
@@ -68,22 +56,9 @@ describe("PropertyView", () => {
     );
 
     const updateButton = screen.getAllByText(/Update Property Two/i)[0];
-    fireEvent.click(updateButton); // Click the first button
+    fireEvent.click(updateButton);
 
-    const propertyTwoElement = screen.getAllByText(/Property Two:/i)[0];
-    expect(propertyTwoElement).toHaveTextContent("Property Two: 100"); // Adjust based on your reducer logic
-
-    // Assert the updated state
-    const stateAfterUpdate = reducer(initialState, {
-      type: ACTION_TYPE_TWO,
-      payload: 100,
-    });
-    expect(stateAfterUpdate.propertyTwo).toBe(100); // Expected value after update
-  });
-
-  it("handles error on non-existing action", () => {
-    expect(() => {
-      reducer(initialState, { type: "UNKNOWN_ACTION", payload: 0 });
-    }).toThrow("Unknown action: UNKNOWN_ACTION");
+    const propertyTwoElements = screen.getAllByText(/Property Two:/i);
+    expect(propertyTwoElements[0]).toHaveTextContent("100"); // Adjust based on your reducer logic
   });
 });
