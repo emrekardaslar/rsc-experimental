bbd1387a61a2efb821693acd866ae7643455a50f
emrekardaslar <emre.kardaslar@gmail.com>
2024-09-22 11:48:54 +0300

    add remix dev tools dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,14 @@
 import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { remixDevTools } from "remix-development-tools";
 
 export default defineConfig({
   server: {
     port: 3000,
   },
   plugins: [
+    // remixDevTools(), use this line to activate remix dev tools
     remix({
       future: {
         v3_fetcherPersist: true,
